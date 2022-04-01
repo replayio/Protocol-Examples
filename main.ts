eaf90c4f039ad8a7c2bdd1d1a095afd3b0f79c51
Mark Erikson <mark@isquaredsoftware.com>
2022-04-01 13:29:22 -0400

    Query for source file contents and hit counts

--- main.ts
+++ main.ts
@@ -21,7 +21,35 @@ function processRecording(recordingId: string) {
     client.Debugger.addNewSourceListener(source => sources.push(source));
     await client.Debugger.findSources({}, sessionId);
 
-    console.log("Sources: ", sources);
+    const demoSourceEntry = sources.find(s => s.url?.endsWith("demo-script.js"))!;
+
+    console.log("Demo source entry: ", demoSourceEntry);
+
+    const demoSourceText = await client.Debugger.getSourceContents(
+      {
+        sourceId: demoSourceEntry.sourceId,
+      },
+      sessionId
+    );
+
+    // console.log("Demo source: ", demoSourceText.contents);
+    const { lineLocations } = await client.Debugger.getPossibleBreakpoints(
+      {
+        sourceId: demoSourceEntry.sourceId,
+      },
+      sessionId
+    );
+
+    const hitCounts = await client.Debugger.getHitCounts(
+      {
+        sourceId: demoSourceEntry.sourceId,
+        locations: lineLocations,
+        maxHits: 250,
+      },
+      sessionId
+    );
+
+    console.log("Hit counts: ", hitCounts.hits.slice(0, 10));
 
     process.exit(0);
   });

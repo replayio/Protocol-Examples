a8341e1c616af1bf9a4619151bcfa46f499aaa14
Mark Erikson <mark@isquaredsoftware.com>
2022-04-01 12:53:39 -0400

    Set up main function scaffolding

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-console.log("test");
+import { startClient } from "./protocol/socket";
+
+async function main() {
+  const [node, script, recordingId] = process.argv;
+
+  if (!recordingId || typeof recordingId !== "string") {
+    console.error("Please provide a Replay recording ID");
+    process.exit(1);
+  }
+
+  processRecording(recordingId);
+}
+
+async function processRecording(recordingId: string) {
+  console.log("Processing recording: ", recordingId);
+}
+
+main();

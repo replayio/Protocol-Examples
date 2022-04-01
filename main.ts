d0483c349b7c307debfcecb73fde882b5dd052e9
Mark Erikson <mark@isquaredsoftware.com>
2022-04-01 14:30:28 -0400

    Create a file coverage entry

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 import { startClient } from "./protocol/socket";
 import type { newSource } from "@recordreplay/protocol";
+import groupBy from "lodash/groupBy";
+import { createFileCoverage, createCoverageMap } from "istanbul-lib-coverage";
+import util from "util";
 
 async function main() {
   const [node, script, recordingId] = process.argv;
@@ -49,7 +52,41 @@ function processRecording(recordingId: string) {
       sessionId
     );
 
-    console.log("Hit counts: ", hitCounts.hits.slice(0, 10));
+    const hitsByLine = groupBy(hitCounts.hits, entry => entry.location.line);
+    const demoFileCoverage = createFileCoverage(demoSourceEntry.url!);
+
+    const allStatementHits = Object.entries(hitsByLine).flatMap(([key, locations]) => {
+      const statementHits = locations.map((l, index) => {
+        const endColumn = locations[index + 1]?.location.column ?? 255;
+        return {
+          start: l.location.column,
+          end: endColumn,
+          hits: l.hits,
+          line: l.location.line,
+        };
+      });
+      return statementHits;
+    });
+
+    allStatementHits.forEach((statement, index) => {
+      demoFileCoverage.statementMap[index] = {
+        start: {
+          line: statement.line,
+          column: statement.start,
+        },
+        end: {
+          line: statement.line,
+          column: statement.end,
+        },
+      };
+
+      demoFileCoverage.s[index] = statement.hits;
+    });
+
+    // const lineCoverage = demoFileCoverage.getLineCoverage();
+    // console.log("Line coverage: ", lineCoverage);
+    const coverageMap = createCoverageMap();
+    coverageMap.addFileCoverage(demoFileCoverage);
 
     process.exit(0);
   });
